07d6617678b5a245c74a527d2038b8b3af1d57f0
Анатолий Куншин <reyiby2015@gmail.com>
2022-12-04 17:31:14 +0300

    Add simple templates

--- testing/app/urls.py
+++ testing/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import index
 
 urlpatterns = [
-    # path('', )
+    path('testing/<int:question_id>/', index, name='index'),
 ]
--- testing/app/views.py
+++ testing/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Question, Category, AnswerVariant
 
 # Create your views here.
+def index(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    category = question.category
+    answer_variants = AnswerVariant.objects.filter(question=question_id)
+
+    context = {
+        'question': question,
+        'category': category,
+        'answer_variants': answer_variants,
+    }
+    return render(request, 'app/index.html', context)
